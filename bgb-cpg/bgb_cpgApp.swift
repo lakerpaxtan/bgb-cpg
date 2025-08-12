7da1f07f2425352f761f376fac39f23f4a238105
Paxtan Laker <paxtanlaker@berkeley.edu>
2025-08-11 21:50:56 -0400

    Initial Commit

---
+++ bgb-cpg/bgb_cpgApp.swift
@@ -0,0 +1,17 @@
+//
+//  bgb_cpgApp.swift
+//  bgb-cpg
+//
+//  Created by Paxtan Laker on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct bgb_cpgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
